6dfb8c5e6753a863740ea6e47faf0833ba34e891
Maria Grimaldi <maria.grimaldi@edunext.co>
2022-01-28 09:26:24 -0400

    feat: add certificate, course, enrollment and cohort extra steps

--- openedx_filters_samples/samples/pipeline.py
+++ openedx_filters_samples/samples/pipeline.py
@@ -6,7 +6,14 @@ Filters steps exemplifying how to:
 """
 from openedx_filters import PipelineStep
 from openedx_filters.learning.filters import (
+    CertificateCreationRequested,
+    CertificateRenderStarted,
+    CohortChangeRequested,
+    CourseAboutRenderStarted,
     CourseEnrollmentStarted,
+    CourseHomeRenderStarted,
+    CourseUnenrollmentStarted,
+    DashboardRenderStarted,
     StudentLoginRequested,
     StudentRegistrationRequested,
 )
@@ -80,6 +87,55 @@ class ModifyModeBeforeEnrollment(PipelineStep):
     def run_filter(self, user, course_key, mode):  # pylint: disable=arguments-differ, unused-argument
         return {
             "mode": "honor",
+        }
+
+
+class ModifyCertificateModeBeforeCreation(PipelineStep):
+    """
+    Change certificate mode from 'honor' to 'no-id-professional'.
+
+    Example usage:
+
+    Add the following configurations to your configuration file:
+
+        "OPEN_EDX_FILTERS_CONFIG": {
+            "org.openedx.learning.certificate.creation.requested.v1": {
+                "fail_silently": False,
+                "pipeline": [
+                    "openedx_filters_samples.samples.pipeline.ModifyCertificateModeBeforeCreation"
+                ]
+            }
+        }
+    """
+    def run_filter(self, user, course_id, mode, status):  # pylint: disable=arguments-differ
+        if mode == 'honor':
+            return {
+                'mode': 'no-id-professional',
+            }
+        return {}
+
+
+class ModifyContextBeforeRender(PipelineStep):
+    """
+    Modify template context before rendering.
+
+    Example usage:
+
+    Add the following configurations to your configuration file:
+
+        "OPEN_EDX_FILTERS_CONFIG": {
+            "org.openedx.learning.certificate.render.started.v1": {
+                "fail_silently": False,
+                "pipeline": [
+                    "openedx_filters_samples.samples.pipeline.ModifyContextBeforeRender"
+                ]
+            }
+        }
+    """
+    def run_filter(self, context):  # pylint: disable=arguments-differ
+        context['context_modified'] = True
+        return {
+            'context': context,
         }
 
 
@@ -171,3 +227,154 @@ class StopLogin(PipelineStep):
         raise StudentLoginRequested.PreventLogin(
             "You can't login on this site.", redirect_to="", error_code="pre-register-login-forbidden"
         )
+
+
+class StopUnenrollment(PipelineStep):
+    """
+    Stop un-enrollment process raising StopUnenrollment exception.
+
+    Example usage:
+
+    Add the following configurations to your configuration file:
+
+        "OPEN_EDX_FILTERS_CONFIG": {
+            "org.openedx.learning.course.unenrollment.started.v1": {
+                "fail_silently": false,
+                "pipeline": [
+                    "openedx_filters_samples.samples.pipeline.StopUnenrollment"
+                ]
+            }
+        }
+    """
+    def run_filter(self, enrollment):  # pylint: disable=arguments-differ
+        raise CourseUnenrollmentStarted.PreventUnenrollment(
+            "You can't un-enroll from this site."
+        )
+
+
+class StopCertificateCreation(PipelineStep):
+    """
+    Stop certificate generation process raising PreventCertificateCreation exception.
+
+    Example usage:
+
+    Add the following configurations to your configuration file:
+
+        "OPEN_EDX_FILTERS_CONFIG": {
+            "org.openedx.learning.certificate.creation.requested.v1": {
+                "fail_silently": False,
+                "pipeline": [
+                    "openedx_filters_samples.samples.pipeline.StopCertificateCreation"
+                ]
+            }
+        }
+    """
+    def run_filter(self, user, course_id, mode, status):  # pylint: disable=arguments-differ
+        raise CertificateCreationRequested.PreventCertificateCreation(
+            "You can't generate a certificate from this site."
+        )
+
+
+class StopCourseAboutRendering(PipelineStep):
+    """
+    Stop course about render raising PreventCourseAboutRender exception.
+
+    Example usage:
+
+    Add the following configurations to your configuration file:
+
+        "OPEN_EDX_FILTERS_CONFIG": {
+            "org.openedx.learning.course_about.render.started.v1": {
+                "fail_silently": False,
+                "pipeline": [
+                    "openedx_filters_samples.samples.pipeline.StopCourseAboutRendering"
+                ]
+            }
+        }
+    """
+    def run_filter(self, context):  # pylint: disable=arguments-differ
+        raise CourseAboutRenderStarted.PreventCourseAboutRender("You can't view this course.")
+
+
+class StopCourseHomeRendering(PipelineStep):
+    """
+    Stop course home render raising StopCourseHomeRendering exception.
+
+    Example usage:
+
+    Add the following configurations to your configuration file:
+
+        "OPEN_EDX_FILTERS_CONFIG": {
+            "org.openedx.learning.course_home.render.started.v1": {
+                "fail_silently": False,
+                "pipeline": [
+                    "openedx_filters_samples.samples.pipeline.StopCourseHomeRendering"
+                ]
+            }
+        }
+    """
+    def run_filter(self, context):  # pylint: disable=arguments-differ
+        raise CourseHomeRenderStarted.PreventCourseHomeRender("You can't view this course.")
+
+
+class StopDashboardRender(PipelineStep):
+    """
+    Stop dashboard render raising PreventDashboardRender exception.
+
+    Example usage:
+
+    Add the following configurations to your configuration file:
+
+        "OPEN_EDX_FILTERS_CONFIG": {
+            "org.openedx.learning.dashboard.render.started.v1": {
+                "fail_silently": False,
+                "pipeline": [
+                    "openedx_filters_samples.samples.pipeline.StopDashboardRender"
+                ]
+            }
+        }
+    """
+    def run_filter(self, context):  # pylint: disable=arguments-differ
+        raise DashboardRenderStarted.PreventDashboardRender("You can't access the dashboard right now.",)
+
+
+class StopCertificateRender(PipelineStep):
+    """
+    Stop certificate render raising PreventCertificateRender exception.
+
+    Example usage:
+
+    Add the following configurations to your configuration file:
+
+        "OPEN_EDX_FILTERS_CONFIG": {
+            "org.openedx.learning.certificate.render.started.v1": {
+                "fail_silently": False,
+                "pipeline": [
+                    "openedx_filters_samples.samples.pipeline.StopCertificateRender"
+                ]
+            }
+        }
+    """
+    def run_filter(self, context):  # pylint: disable=arguments-differ
+        raise CertificateRenderStarted.PreventCertificateRender("You can't view this certificate.")
+
+
+class StopCohortChange(PipelineStep):
+    """
+    Stop cohort change process raising PreventCohortChange exception.
+
+    Example usage:
+
+    Add the following configurations to your configuration file:
+
+        "OPEN_EDX_FILTERS_CONFIG": {
+            "org.openedx.learning.cohort.change.requested.v1": {
+                "fail_silently": False,
+                "pipeline": [
+                    "openedx_filters_samples.samples.pipeline.StopCohortChange"
+                ]
+            }
+        }
+    """
+    def run_filter(self, current_cohort, previous_cohort):  # pylint: disable=arguments-differ
+        raise CohortChangeRequested.PreventCohortChange("You can't change cohorts.")
