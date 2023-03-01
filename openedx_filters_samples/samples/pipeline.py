87036f3c9ecd13d37aac76b96f5a78befabe6335
henrrypg <henrry.pulgarin@edunext.co>
2023-03-01 11:46:42 -0500

    fix: quality

--- openedx_filters_samples/samples/pipeline.py
+++ openedx_filters_samples/samples/pipeline.py
@@ -10,6 +10,7 @@ from openedx_filters.learning.filters import (
     AccountSettingsRenderStarted,
     CertificateCreationRequested,
     CertificateRenderStarted,
+    CohortAssignmentRequested,
     CohortChangeRequested,
     CourseAboutRenderStarted,
     CourseEnrollmentStarted,
@@ -17,7 +18,6 @@ from openedx_filters.learning.filters import (
     DashboardRenderStarted,
     StudentLoginRequested,
     StudentRegistrationRequested,
-    CohortAssignmentRequested,
 )
 
 
@@ -121,7 +121,7 @@ class ModifyCertificateModeBeforeCreation(PipelineStep):
             }
         }
     """
-    def run_filter(self, user, course_id, mode, status, *args, **kwargs):  # pylint: disable=arguments-differ, unused-argument
+    def run_filter(self, user, course_id, mode, status, *args, **kwargs):  # pylint: disable=arguments-differ
         if mode == 'honor':
             return {
                 'mode': 'no-id-professional',
@@ -197,7 +197,7 @@ class ModifyUserProfileBeforeCohortChange(PipelineStep):
         user = current_membership.user
         user.profile.set_meta(
             {
-                "cohort_info": f"Changed from Cohort {str(current_membership.course_user_group)} to Cohort {str(target_cohort)}"
+                "cohort_info": f"Changed from Cohort {str(current_membership.course_user_group)} to Cohort {str(target_cohort)}"  # pylint: disable=line-too-long
             }
         )
         user.profile.save()
@@ -244,7 +244,7 @@ class StopEnrollment(PipelineStep):
         }
     """
 
-    def run_filter(self, *args, **kwargs):  # pylint: disable=arguments-differ
+    def run_filter(self, *args, **kwargs):
         raise CourseEnrollmentStarted.PreventEnrollment("You can't enroll on this course.")
 
 
@@ -266,7 +266,7 @@ class StopRegister(PipelineStep):
         }
     """
 
-    def run_filter(self, *args, **kwargs):  # pylint: disable=arguments-differ
+    def run_filter(self, *args, **kwargs):
         raise StudentRegistrationRequested.PreventRegistration("You can't register on this site.", status_code=403)
 
 
@@ -643,7 +643,7 @@ class RenderCustomCertificateStep(PipelineStep):
         Pipeline steps that gets or creates a new custom template to render instead
         of the original.
         """
-        from opaque_keys.edx.keys import CourseKey
+        from opaque_keys.edx.keys import CourseKey  # pylint: disable=import-outside-toplevel
 
         course_key = CourseKey.from_string(context["course_id"])
         custom_template = self._get_or_create_custom_template(mode='honor', course_key=course_key)
@@ -653,7 +653,7 @@ class RenderCustomCertificateStep(PipelineStep):
         """
         Creates a custom certificate template entry in DB.
         """
-        from lms.djangoapps.certificates.models import CertificateTemplate
+        from lms.djangoapps.certificates.models import CertificateTemplate  # pylint: disable=E0401, C0415
 
         template_html = """
             <%namespace name='static' file='static_content.html'/>
@@ -752,6 +752,7 @@ class StaffViewCourseAbout(PipelineStep):
         return {
             "context": context, template_name: template_name,
         }
+
 
 class StopAccountSettingsRender(PipelineStep):
     """
